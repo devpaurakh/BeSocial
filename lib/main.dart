c4141a8cfa1aff1c7f6f63c0cf2816779802420a
Paurakh Saud <paurakhsaud2022@gmail.com>
2024-07-13 00:14:58 +0545

    Starting to create a folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:besocial/screens/homepage.screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized;
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -20,4 +23,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
