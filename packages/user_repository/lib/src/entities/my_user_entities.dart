d027c748a02b9dd4929083f2fab4c874667b6d0f
Paurakh Saud <paurakhsaud2022@gmail.com>
2024-07-14 00:12:33 +0545

    created model extended to equatables for the bloc

--- packages/user_repository/lib/src/entities/my_user_entities.dart
+++ packages/user_repository/lib/src/entities/my_user_entities.dart
@@ -1,3 +1,46 @@
-class MyUserEntities{
-  
+import 'package:equatable/equatable.dart';
+
+class MyUserEntities extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+  final String? image;
+
+  const MyUserEntities({
+    required this.id,
+    required this.email,
+    required this.name,
+    this.image,
+  });
+
+  Map<String, Object?> toDocument() {
+    return {
+      'id': id,
+      'email': email,
+      'name': name,
+      'image': image,
+    };
+  }
+
+  static MyUserEntities fromDocument(Map<String, dynamic> doc) {
+    return MyUserEntities(
+        id: doc['id'] as String,
+        email: doc['email'] as String,
+        name: doc['name'] as String,
+        image: doc['image'] as String);
+  }
+
+
+  @override
+  String toString(){
+    return '''UserEntity:(
+    id : $id,
+    email: $email,
+    name: $name,
+    image:$image,
+    )''';
+  }
+
+  @override
+  List<Object?> get props => [id, email, name, image];
 }
--- packages/user_repository/lib/src/models/my_user_models.dart
+++ packages/user_repository/lib/src/models/my_user_models.dart
@@ -1,1 +1,61 @@
-class MyUserModels{}
+import 'package:equatable/equatable.dart';
+
+import '../entities/my_user_entities.dart';
+
+class MyUserModels extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+  final String? image;
+
+  const MyUserModels({
+    required this.id,
+    required this.email,
+    required this.name,
+    this.image,
+  });
+
+  static const empty = MyUserModels(
+      id: "",
+      email: "",
+      name: "",
+      image:
+          ""); // this is the empty method called MyuserModel which modify the myusermodel paramater
+
+//this is copy with method of the usermodel
+  MyUserModels copyWith(
+      {String? id, String? email, String? name, String? image}) {
+    return MyUserModels(
+        id: id ?? this.id,
+        email: email ?? this.email,
+        name: name ?? this.name,
+        image: image ?? this.image);
+  }
+
+  //getter to determin if the current user is empty
+  bool get isEmpty => this == MyUserModels.empty;
+
+  //getter to determin if the current user is not empty
+
+  bool get isNotEmpty => this != MyUserModels.empty;
+
+  MyUserEntities toUserEntity() {
+    return MyUserEntities(
+      id: id,
+      email: email,
+      name: name,
+      image: image,
+    );
+  }
+
+  static MyUserModels fromEitity(MyUserEntities entities) {
+    return MyUserModels(
+        id: entities.id,
+        email: entities.email,
+        name: entities.name,
+        image: entities.image);
+  }
+
+  @override
+  List<Object?> get props => [id, email, name, image];
+}
