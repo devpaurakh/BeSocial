872e4a093290f028e72f6583c1197006074bb0ef
Paurakh Saud <paurakhsaud2022@gmail.com>
2024-08-05 09:14:25 +0545

    Some change in userrepo

--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -1,18 +1,12 @@
-
-
 import 'package:user_repository/user_repository.dart';
 
-abstract class UserRepo{
-
-
+abstract class UserRepo {
   Future<void> signIn(String youremail, String password);
 
   Future<void> signOut();
 
   Future<MyUserModels> signUp(MyUserModels myUser, String password);
 
-
-  Future<void> reSetPassword(String yourEmail); //Leter change this this is only for commit
-
-  
+  Future<void> reSetPassword(
+      String yourEmail); //Leter change this this is only for commit
 }
